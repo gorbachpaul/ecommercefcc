3f511de0f1d02fbce37ff1d2a862fb789c970614
calledandrey <andrey0405@list.ru>
2017-06-03 11:50:51 +0300

    add npm start

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const 	express = require('express')
 
 
 app.get('/', (req, res) => {
-	res.send('index.html')
+	res.sendFile(__dirname+"/index.html")
 })
 
-app.listen(3000, '127.0.0.1', () => {
-    console.log('Server has started on port:', port)
+app.listen(3033, '127.0.0.1', () => {
+    console.log('Server has started')
 })
