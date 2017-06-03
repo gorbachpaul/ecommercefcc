6a2a548580a8b9d23a8697be0922a6212f7f0608
calledandrey <andrey0405@list.ru>
2017-06-03 13:39:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname+"/index.html")
 })
 
-app.listen(80, '127.0.0.1', () => {
+app.listen(80, '0.0.0.0', () => {
     console.log('Server has started')
 })
