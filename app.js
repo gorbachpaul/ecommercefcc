132e1c0541af9f0312e0fe1ec7155e8d01b51ce2
calledandrey <andrey0405@list.ru>
2017-06-03 12:46:57 +0300

    fix port to 80

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname+"/index.html")
 })
 
-app.listen(3033, '127.0.0.1', () => {
+app.listen(80, '127.0.0.1', () => {
     console.log('Server has started')
 })
