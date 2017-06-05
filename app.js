0fb0cdcc621557c386db352b6975f8de280bff0e
Ivan <subtlety.86@gmail.com>
2017-06-05 17:14:54 +0300

    fixed 5000 port hardcode

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname+"/index.html")
 })
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Server has started')
 })
